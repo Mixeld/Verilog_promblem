5665820ad969a87c907329b5b8d2a0ae3fcc3fa7
Mixeld <87525307+Mixeld@users.noreply.github.com>
2026-03-31 11:28:00 +0300

    Add files via upload

--- JK_flip_flop.sv
+++ JK_flip_flop.sv
@@ -26,22 +26,4 @@ end
 endmodule
 
 
-module d_flip_flop(
-    input logic clk,
-    input logic rst_n,
-    input logic D,
-    output logic Q,
-    output logic Q_n
-);
 
-always_ff @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        Q <= 1'b0;
-        Q_n <= 1'b1;
-    end else begin
-        Q <= D;
-        Q_n <= ~D;
-    end
-end
-
-endmodule
